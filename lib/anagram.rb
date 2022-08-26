985403244861d017fac074f0b8837d7e8c832e2f
Jobsidney <jobsidney67@gmail.com>
2022-08-26 13:17:46 +0300

    commit tree 4bd61b8f0a4acf04fc3556e94a948e241991fffa

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
 # Your code goes here!
+
+class Anagram
+    attr_accessor :word
+    def initialize=(word:)
+        @word=word
+    end
+    def match(arr)
+        arr.find{
+            |item|
+            item==word
+        }
+
+    end
+end
